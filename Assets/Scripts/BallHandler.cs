49eb8d76de12dba9ce52e8395e18f8f1f5c0ff5e
Ilya <ilya.balashow1@gmail.com>
2021-12-20 19:52:39 +0600

    Transforms a point from screen space into world space

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private Camera mainCamera;
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -18,6 +20,12 @@ public class BallHandler : MonoBehaviour
             return;
 
        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        Debug.Log("TouchPosition");
+
+        //	Transforms a point from screen space into world space,
+        //  where world space is defined
+        //  as the coordinate system at the very top of your game's hierarchy.
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+        Debug.Log(worldPosition);
     }
 }
