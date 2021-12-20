5dd03a107c9b9febf4270de6fd9cfb5e2101dc35
Ilya <ilya.balashow1@gmail.com>
2021-12-20 23:13:51 +0600

    added Launching  the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,12 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D currentBallRigidBody;
+    [SerializeField] private SpringJoint2D currentBallSprintJoint;
+    [SerializeField] private float detachDelay = 0.25f;
 
     private Camera mainCamera;
+    private bool isDragging;
     void Start()
     {
         mainCamera = Camera.main;
@@ -16,16 +21,45 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(currentBallRigidBody == null) { return; }
         if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
+            
             return;
+        }
 
-       Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        isDragging = true;
+
+        currentBallRigidBody.isKinematic = true;
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         //	Transforms a point from screen space into world space,
         //  where world space is defined
         //  as the coordinate system at the very top of your game's hierarchy.
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-        Debug.Log(worldPosition);
+
+        currentBallRigidBody.position = worldPosition;
+        
+        
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSprintJoint.enabled = false;
+        currentBallSprintJoint = null;
     }
 }
